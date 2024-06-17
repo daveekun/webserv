219378f573474978a3fed738387bef16297904e5
dhorvath <david.horvath@gmx.de>
2024-06-17 15:24:23 +0300

    removed exception

--- src/Route.cpp
+++ src/Route.cpp
@@ -1,14 +1,15 @@
 #include "Route.hpp"
 
 Route::Route(std::list<e_HTTP_methods> allowed_methods,
-        std::string location, bool dir_listing,
-        std::string cgi, bool accept_upload = false,
-        std::string upload_dir) : m_location(location),
+        std::string location, std::string cgi, 
+        std::string upload_dir,
+        bool accept_upload, bool dir_listing) 
+        : m_location(location),
         m_dir_listing(dir_listing), m_CGI(cgi), 
         m_accept_upload(accept_upload), m_uploaddir(upload_dir)
 {
     for (e_HTTP_methods& method : allowed_methods)
-        m_allowed_methods += method;
+        m_allowed_methods |= method;
     validate();
 }
 
@@ -21,10 +22,6 @@ bool Route::validate() const
     if (m_uploaddir != "" && m_accept_upload == false)
         throw InvalidUploadException();
     return true;
-}
-
-const char * Route::InvalidConfigException::what() const noexcept {
-    return "Invalid configuration in route";
 }
 
 const char * Route::InvalidMethods::what() const noexcept {
