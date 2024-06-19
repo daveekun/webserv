ff83882c475674d195dd2df7c9d582937f7cf5a5
david <dhorvath@student.hive.fi>
2024-06-19 10:11:07 +0300

    tokeniser removed for mac compilation

--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -9,54 +9,55 @@ ServerConfig::ServerConfig(std::string path) : m_file_path(path)
         throw InvalidPathException();
 
     int depth = 0;
-    for (std::string line; config >> line;)
-    {
-        // # at start of line is comment
-        // each line contains
-        // key: value,
-        //      or
-        // key { 
-        // except the last line of each block, where there is no ","
-        // and the end of blocks, where its only "}" 
-        if (line._Starts_with("server"))
-        {
-            if (line.begin() + line.find("{") != line.end()
-            || line.rfind("{") != line.find("{")
-            || line.find_first_not_of(" ", line.find("server") + 6, line.find("{")) == std::string::npos)
-                throw InvalidServerException();
-            depth++;
-        }
-        else if (line._Starts_with("name:"))
-        {
+	/* TODO tokeniser that makes dict*/
+    // for (std::string line; config >> line;)
+    // {
+    //     // # at start of line is comment
+    //     // each line contains
+    //     // key: value,
+    //     //      or
+    //     // key { 
+    //     // except the last line of each block, where there is no ","
+    //     // and the end of blocks, where its only "}" 
+    //     if (line._Starts_with("server"))
+    //     {
+    //         if (line.begin() + line.find("{") != line.end()
+    //         || line.rfind("{") != line.find("{")
+    //         || line.find_first_not_of(" ", line.find("server") + 6, line.find("{")) == std::string::npos)
+    //             throw InvalidServerException();
+    //         depth++;
+    //     }
+    //     else if (line._Starts_with("name:"))
+    //     {
             
-        }
-        else if (line._Starts_with("host:"))
-        {
+    //     }
+    //     else if (line._Starts_with("host:"))
+    //     {
 
-        }
-        else if (line._Starts_with("port:"))
-        {
+    //     }
+    //     else if (line._Starts_with("port:"))
+    //     {
 
-        }
-        else if (line._Starts_with("errorpages:"))
-        {
+    //     }
+    //     else if (line._Starts_with("errorpages:"))
+    //     {
 
-        }
-        else if (line._Starts_with("sizelimit:"))
-        {
+    //     }
+    //     else if (line._Starts_with("sizelimit:"))
+    //     {
 
-        }
-        else if (line._Starts_with("route"))
-        {
+    //     }
+    //     else if (line._Starts_with("route"))
+    //     {
 
-        }
-        else if (line._Starts_with("}"))
-        {
+    //     }
+    //     else if (line._Starts_with("}"))
+    //     {
 
-        }
-        else
-            throw InvalidSettingException();
-    }
+    //     }
+    //     else
+    //         throw InvalidSettingException();
+    // }
 }
 
 const char * ServerConfig::InvalidPathException::what() const noexcept
